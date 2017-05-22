95731bd4e33d9b622f34e3d0ea0bfb487f53fdef
RyleeAndrews <ryleeandrews73@gmail.com>
2017-05-22 07:10:36 -0700

    final todos finished

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -83,7 +83,10 @@ var app = app || {};
     // The first property should be pretty straightforward, but you will need to chain
     // some combination of filter, map, and reduce to get the value for the second
     // property.
-
+    return {
+      author: this.author,
+      words: this.numWordsAll()
+    }
     })
   };
 
